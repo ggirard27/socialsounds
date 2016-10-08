e3f7fc0e35c0749a71f025727855c13333a528dd
Guillaume Roy-Girard <guillaume.roy.girard@outlook.com>
2016-10-08 19:57:35 -0400

    Add project files.

---
+++ socialsounds/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(socialsounds.Startup))]
+namespace socialsounds
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
